660cb4003cd95d505b784e67575880682059adea
steveroger241 <csjm2k20241@gmail.com>
2024-04-22 23:41:03 +0530

    added express.static midlwre

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/form', formroutes);
 app.use('/auth', authroutes);
